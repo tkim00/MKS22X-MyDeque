6804bf8799654f2a8a35c086fbbd363f68545b8f
Taejoon Kim <tkim00@stuy.edu>
2019-03-22 11:04:01 -0400

    finished writing both addFirst and addLast

--- MyDeque.java
+++ MyDeque.java
@@ -43,6 +43,12 @@ public class MyDeque<E>{
     return str;
   }
   public void addFirst(E element){
+    if (element == null) {
+      throw new NullPointerException();
+    }
+    // if (size > data.length) {
+    //   resize();
+    // }
     if (start > 0) {
       data[start-1] = element;
       start--;
@@ -53,7 +59,23 @@ public class MyDeque<E>{
       size++;
     }
   }
-  public void addLast(E element){ }
+  public void addLast(E element){
+    if (element == null) {
+      throw new NullPointerException();
+    }
+    // if (size > data.length) {
+    //   resize();
+    // }
+    if (end < data.length-1) {
+      data[end+1] = element;
+      end++;
+      size++;
+    } else {
+      data[0] = element;
+      end = 0;
+      size++;
+    }
+  }
   public E removeFirst(){ }
   public E removeLast(){ }
   public E getFirst(){ }
