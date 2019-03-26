e9375b1b2f74cd973638d924a95a0aed01da38bb
Taejoon Kim <tkim00@stuy.edu>
2019-03-26 01:13:25 -0400

    finished creating the eval method

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,35 @@
+import java.lang.*;
+import java.util.*;
+
 public class Calculator {
-  
+  /*Evaluate a postfix expression stored in s.
+   *Assume valid postfix notation, separated by spaces.
+   */
+  public static double eval(String s){
+    MyDeque<Double> deque = new MyDeque<>();
+    String[] tokens = s.split();
+    for (int i = 0; i < tokens.length; i++) {
+      if (tokens[i].equals("+")) {
+        deque.addLast(deque.removeLast() + deque.removeLast());
+      }
+      if (tokens[i].equals("-")) {
+        deque.addLast(-1*deque.removeLast() + deque.removeLast());
+      }
+      if (tokens[i].equals("*")) {
+        deque.addLast(deque.removeLast() * deque.removeLast());
+      }
+      if (tokens[i].equals("/")) {
+        deque.addLast(1/deque.removeLast() * deque.removeLast());
+      }
+      if (tokens[i].equals("%")) {
+        Double y = deque.removeLast();
+        Double x = deque.removeLast();
+        deque.addLast(x % y);
+      }
+      else {
+        deque.addLast(Double.parseDouble(tokens[i]));
+      }
+    }
+    return deque.removeLast();
+  }
 }
