8807c832300cf7f6b820fb471e08c4c691c3bde0
Taejoon Kim <tkim00@stuy.edu>
2019-04-01 03:48:55 -0400

    Calculator tested and working

--- Calculator.java
+++ Calculator.java
@@ -5,23 +5,27 @@ public class Calculator {
   /*Evaluate a postfix expression stored in s.
    *Assume valid postfix notation, separated by spaces.
    */
+  public static void main(String[] args) {
+    System.out.println(eval("1 2 3 4 5 + * - -"));
+  }
   public static double eval(String s){
     MyDeque<Double> deque = new MyDeque<>();
-    String[] tokens = s.split();
+    String[] tokens = s.split(" ");
     for (int i = 0; i < tokens.length; i++) {
+      //System.out.println(tokens.length);
       if (tokens[i].equals("+")) {
         deque.addLast(deque.removeLast() + deque.removeLast());
       }
-      if (tokens[i].equals("-")) {
+      else if (tokens[i].equals("-")) {
         deque.addLast(-1*deque.removeLast() + deque.removeLast());
       }
-      if (tokens[i].equals("*")) {
+      else if (tokens[i].equals("*")) {
         deque.addLast(deque.removeLast() * deque.removeLast());
       }
-      if (tokens[i].equals("/")) {
+      else if (tokens[i].equals("/")) {
         deque.addLast(1/deque.removeLast() * deque.removeLast());
       }
-      if (tokens[i].equals("%")) {
+      else if (tokens[i].equals("%")) {
         Double y = deque.removeLast();
         Double x = deque.removeLast();
         deque.addLast(x % y);
